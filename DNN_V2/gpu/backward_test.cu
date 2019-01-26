f8d6403a4d07c6d9baecb4d46edb72a10c6d35a5
minsukji <minsuk.ji@gmail.com>
2019-01-26 09:32:18 -0600

    Modify backward_test case to better test dropout operation

--- DNN_V2/gpu/backward_test.cu
+++ DNN_V2/gpu/backward_test.cu
@@ -18,8 +18,7 @@ TEST_CASE("backward propagation is computed", "[backwardProp]") {
   float *W = new float[8+4] {1.2f, -1.8f, -0.7f, 0.14f,
                              2.88f, 0.3f, 0.85f, -0.56f,
                              0.34f, 4.4f, -0.42f, -3.6f};
-  float *B = new float[4+1] {0.1f, 0.2f, 0.67f, -0.3f, 0.15f};
-  //float *B = new float[4+1] {0.1f, 0.2f, 0.67f, 0.3f, 0.15f};
+  float *B = new float[4+1] {0.1f, 0.2f, 0.67f, 0.3f, 0.15f};
   float *dW = new float[8+4] {};
   float *dB = new float[4+1] {};
   float *d_X, *d_W, *d_B, *d_Z, *d_A, *d_dW, *d_dB, *d_dZ, *d_dA;
@@ -54,12 +53,12 @@ TEST_CASE("backward propagation is computed", "[backwardProp]") {
     float *layer_drop = new float[n_layers+1] {1.0f, 1.0f, 1.0f};
     float *d_D = nullptr;
     float *Z
-      = new float[(4+1)*batch_size] {2.236f, -0.925f, 0.5625f, -0.454f, 1.468f,
-                                     -1.39f, 0.125f, -0.23f, 0.67399f, 0.59662f};
+      = new float[(4+1)*batch_size] {2.236f, -0.925f, 0.5625f, 0.146f, 1.468f,
+                                     -1.39f, 0.125f, 0.37f, 0.14839f, -0.73538f};
     float *A
-      = new float[(4+1)*batch_size] {2.236f, 0.0f, 0.5625f, 0.0f, 1.468f,
-                                     0.0f, 0.125f, 0.0f, 0.662396010418219f,
-                                     0.644882635337264f};
+      = new float[(4+1)*batch_size] {2.236f, 0.0f, 0.5625f, 0.146f, 1.468f,
+                                     0.0f, 0.125f, 0.37f,
+                                     0.537029576908463f, 0.324015235550852f};
     cudaMemcpy(d_Z, Z, ((4+1)*batch_size) * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_A, A, ((4+1)*batch_size) * sizeof(float), cudaMemcpyHostToDevice);
 
@@ -70,11 +69,12 @@ TEST_CASE("backward propagation is computed", "[backwardProp]") {
     cudaMemcpy(dB, d_dB, (4+1) * sizeof(float), cudaMemcpyDeviceToHost);
 
     float *correct_dW
-      = new float[8+4] {-0.094507831553631f, 0.0f, 0.116744968389779f, 0.0f,
-                        -0.031863700402273f, 0.0f, 0.039361041673396f, 0.0f,
-                        -0.638097406014880f, 0.0f, -0.117145957361297f, 0.0f};
+      = new float[8+4] {-0.158519149308613f, 0.0f, 0.195817772675345f, 1.678438051502957f,
+                        -0.046908978362138f, 0.0f, 0.057946385035582f, 0.496683300304992f,
+                        -1.013773750122013f, 0.0f, -0.172459479272566f, -0.158854022308775};
     float *correct_dB
-      = new float[4+1] {-0.117762630221568f, 0.0f, 0.145471484391349f, 0.0f, -0.346360677122259f};
+      = new float[4+1] {-0.193622381881916f, 0.0f, 0.239180589383544f, 2.050119337573233f,
+                        -0.569477593770343f};
 
     for (int i = 0; i < (8+4); ++i)
       REQUIRE(dW[i] == Approx(correct_dW[i]));
@@ -89,12 +89,12 @@ TEST_CASE("backward propagation is computed", "[backwardProp]") {
     float *layer_drop = new float[n_layers+1] {1.0f, 1.0f, 1.0f};
     float *d_D = nullptr;
     float *Z
-      = new float[(4+1)*batch_size] {2.236f, -0.925f, 0.5625f, -0.454f, 1.468f,
-                                     -1.39f, 0.125f, -0.23f, 0.67399f, 0.59662f};
+      = new float[(4+1)*batch_size] {2.236f, -0.925f, 0.5625f, 0.146f, 1.468f,
+                                     -1.39f, 0.125f, 0.37f, 0.14839f, -0.73538f};
     float *A
-      = new float[(4+1)*batch_size] {2.236f, 0.0f, 0.5625f, 0.0f, 1.468f,
-                                     0.0f, 0.125f, 0.0f, 0.662396010418219f,
-                                     0.644882635337264f};
+      = new float[(4+1)*batch_size] {2.236f, 0.0f, 0.5625f, 0.146f, 1.468f,
+                                     0.0f, 0.125f, 0.37f,
+                                     0.537029576908463f, 0.324015235550852f};
     cudaMemcpy(d_Z, Z, ((4+1)*batch_size) * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_A, A, ((4+1)*batch_size) * sizeof(float), cudaMemcpyHostToDevice);
 
@@ -105,11 +105,12 @@ TEST_CASE("backward propagation is computed", "[backwardProp]") {
     cudaMemcpy(dB, d_dB, (4+1) * sizeof(float), cudaMemcpyDeviceToHost);
 
     float *correct_dW
-      = new float[8+4] {-0.03450783155363056f, -0.09f, 0.08174496838977893f, 0.007f,
-                        0.11213629959772721f, 0.015f, 0.08186104167339578f, -0.028f,
-                        -0.621097406014880f, 0.22f, -0.138145957361297f, -0.18f};
+      = new float[8+4] {-0.0985191493086126f, -0.09f, 0.1608177726753449f, 1.6854380515029568f,
+                        0.0970910216378619f, 0.015f, 0.1004463850355823f, 0.4686833003049916f,
+                        -0.996773750122013f, 0.22f, -0.193459479272566f, -0.338854022308775f};
     float *correct_dB
-      = new float[4+1] {-0.117762630221568f, 0.0f, 0.145471484391349f, 0.0f, -0.346360677122259f};
+      = new float[4+1] {-0.193622381881916f, 0.0f, 0.239180589383544f, 2.050119337573233f,
+                        -0.569477593770343f};
 
     for (int i = 0; i < (8+4); ++i)
       REQUIRE(dW[i] == Approx(correct_dW[i]));
@@ -129,15 +130,14 @@ TEST_CASE("backward propagation is computed", "[backwardProp]") {
     // 0.147921, 0.460329, 0.30005, 0.907808, 0.0552578,
     // 0.0109106, 0.251024, 0.263781, 0.391988, 0.72534
     float *Z
-      = new float[(4+1)*batch_size] {2.236f, -0.925f, 0.5625f, -0.454f, 1.468f,
-                                     -1.39f, 0.125f, -0.23f, 0.898557142857143f,
-                                     0.788028571428572f};
+      = new float[(4+1)*batch_size] {2.236f, -0.925f, 0.5625f, 0.146f, 1.468f,
+                                     -1.39f, 0.125f, 0.37f, 0.1477f, -1.114828571428572f};
     float *A
-      = new float[(4+1)*batch_size] {3.19428571428571f, 0.0f,
-                                     0.803571428571429f, 0.0f,
-                                     2.09714285714286f, 0.0f,
-                                     0.178571428571429f, 0.0f,
-                                     0.710652904814677f, 0.687407869971444f};
+      = new float[(4+1)*batch_size] {3.194285714285715f, 0.0f,
+                                     0.803571428571429f, 0.208571428571429f,
+                                     2.097142857142857f, 0.0f,
+                                     0.178571428571429f, 0.528571428571429f,
+                                     0.536858018652687f, 0.246971789920003f}; 
     cudaMemcpy(d_Z, Z, ((4+1)*batch_size) * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_A, A, ((4+1)*batch_size) * sizeof(float), cudaMemcpyHostToDevice);
 
@@ -148,11 +148,12 @@ TEST_CASE("backward propagation is computed", "[backwardProp]") {
     cudaMemcpy(dB, d_dB, (4+1) * sizeof(float), cudaMemcpyDeviceToHost);
 
     float *correct_dW
-      = new float[8+4] {-0.117512714602032f, 0.0f, 0.145162765096628f, 0.0f,
-                        -0.039213027131661f, 0.0f, 0.048439621750875f, 0.0f,
-                        -0.789903822654501f, 0.0f, -0.144165540925224, 0.0f};
+      = new float[8+4] {-0.243324588460814f, 0.0f, 0.300577432804535f, 1.742722429042278f,
+                        -0.068902630206328f, 0.0f, 0.085115013784287f, 0.193635825449142f,
+                        -1.529307773350020f, 0.0f, -0.253318493405617f, -0.247313690718790f};
     float *correct_dB
-      = new float[4+1] {-0.146185240409085f, 0.0f, 0.180581767564164f, 0.0f, -0.300969612606939f};
+      = new float[4+1] {-0.295355617918061f, 0.0f, 0.364851057428193f, 1.936358254491420f,
+                        -0.608085095713655};
 
     for (int i = 0; i < (8+4); ++i)
       REQUIRE(dW[i] == Approx(correct_dW[i]));
